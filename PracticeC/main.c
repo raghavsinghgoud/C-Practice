a6fd9ece95155b4a7c742d7494118800a7bddec8
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-27 21:23:59 +0530

    first commit

---
+++ PracticeC/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is my first program! :)
+
+    /*
+    This
+    is
+    also
+    a comment
+    */
+
+    printf("I like pizza!\n")
+    printf("It's really good!\n")
+
+    return 0;
+}
