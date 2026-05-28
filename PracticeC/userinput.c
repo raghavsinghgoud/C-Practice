ebb67f2518352043a19ba24b58a9f07d5459cc9f
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-28 12:17:29 +0530

    commit

--- PracticeC/userinput.c
+++ PracticeC/userinput.c
@@ -1,7 +1,34 @@
 // #include <stdio.h>
+// #include <string.h>
 
 // int main(){
 
+//     int age = 0;
+//     float gpa = 0.0f;
+//     char grade = '\0';
+//     char name[30]= "";
+
+//     printf("Enter your age: ");
+//     scanf("%d", &age);
+
+//     printf("Enter your gpa: ");
+//     scanf("%f", &gpa);
+
+//     printf("Enter your grade: ");
+//     scanf("%c", &grade);
+
+//     getchar();
+//     printf("Enter your full name: ");
+//     scanf("%s", &name); //only prints name before using any spaces
+//     fgets(name, 30, stdin); //will print full name
+//     fgets(name, sizeof(name), stdin); // will print according to size we assigned at first
+//     name[strlen(name)-1] = '\0';
+
+
+//     printf("%d\n", age);
+//     printf("%.2f\n", gpa);
+//     printf("%c\n", grade);
+//     printf("%s\n", name);
 //     printf("Hello,\n");
 
 //     return 0;
