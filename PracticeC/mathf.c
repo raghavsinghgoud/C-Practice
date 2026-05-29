09ba2c684c5770c11bd3179c25b9df5145d5d43f
raghavsinghgoud <raghavsinghgoud@gmail.com>
2026-05-30 00:30:38 +0530

    mathf

---
+++ PracticeC/mathf.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+// #include <math.h>
+
+// int main() {
+
+//     int x = 9;
+//     float x = 3.14;
+
+//     x = sqrt(x);
+//     x= pow(x,4);
+//     x = round(x);
+//     x = ceil(x);
+//     x = floor(x);
+//     x = abs(x);
+//     x = log(x);
+//     x= sin(x);
+//     x= cos(x);
+//     x= tan(x);
+
+
+//     printf("%f", x);
+
+//     return 0;
+
+// }
